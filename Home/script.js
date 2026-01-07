8c2e46a69db4ad2409f7272d8d426399b6cc7a5c
Phaleap <minhphaleap168@gmail.com>
2026-01-06 20:32:29 -1100

    update

--- Home/script.js
+++ Home/script.js
@@ -9,20 +9,16 @@ const totalPanelWidth = panels.reduce((total, panel, index) => {
 
 // Set up the GSAP animation timeline
 gsap.to(".horizontal-sections", {
-    // Animate the 'x' property (horizontal movement) to the negative of the total scroll width
     x: `-${totalPanelWidth}px`, 
-    ease: "none", // Linear movement
-    
+    ease: "none", 
     scrollTrigger: {
         trigger: ".horizontal-sections-wrapper", // The tall container that drives the scroll
+        id: "horizontalScroll",
         pin: true, // Pin the horizontal-sections to the viewport
         scrub: 1, // Smoothly link scroll to animation
         // Start: when the top of the wrapper hits the top of the viewport
         start: "top top", 
-        // End: when the bottom of the wrapper hits the top of the viewport
         end: "bottom top", 
-        // Optional: shows scroll-trigger markers for debugging
-        // markers: true, 
     }
 });
 
@@ -58,21 +54,6 @@ gsap.to(".highlight-animate", {
     }
 });
 
-// const link = document.querySelector(".hover-follow");
-// const icon = link.querySelector("span");
-
-// link.addEventListener("mousemove", (e) => {
-//     const rect = link.getBoundingClientRect();
-
-//     const x = e.clientX - rect.left - rect.width / 2;
-//     const y = e.clientY - rect.top - rect.height / 2;
-
-//     icon.style.transform = `translate(${x * 0.4}px, ${y * 0.4}px)`;
-// });
-
-// link.addEventListener("mouseleave", () => {
-//     icon.style.transform = "translate(0, 0)";
-// });
 
 
 const link = document.querySelector(".hover-follow");
@@ -87,3 +68,28 @@ link.addEventListener("mousemove", (e) => {
 link.addEventListener("mouseleave", (e) => {
     icon.style.transform = "translate(0, 0)";
 })
+const video = document.getElementById('hoverVideo');
+
+video.addEventListener('mouseenter', () => {
+    video.play();
+});
+
+video.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0; 
+});
+document.querySelectorAll(".hoverVideo").forEach(video => {
+    video.addEventListener("mouseenter", () => {
+        video.play().catch(() => {});
+    });
+
+    video.addEventListener("mouseleave", () => {
+        video.pause();
+        video.currentTime = 0;
+    });
+});
+
+
+
+
+
